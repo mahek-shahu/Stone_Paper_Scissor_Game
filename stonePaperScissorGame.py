0798795f3fdb08c6f1ee1cc044169506af73d97c
Mahek <128591145+mahek-shahu@users.noreply.github.com>
2023-05-07 02:41:01 +0530

    Add files via upload

---
+++ stonePaperScissorGame.py
@@ -0,0 +1,21 @@
+import random
+user_action=input("User Turn : ")
+computer_action=input("Computer Turn : ")
+print(f"\nUser Chooose - {user_action} and Computer Choose - {computer_action}\n")
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
